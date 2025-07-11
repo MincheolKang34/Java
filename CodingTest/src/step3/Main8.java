ffb7bf52e4a044b1e27734db5e78b1e9ca2a78d5
mincheolkang34 <mincheolkang34@gmail.com>
2025-07-11 11:20:21 +0900

    add java projects

---
+++ CodingTest/src/step3/Main8.java
@@ -0,0 +1,25 @@
+package step3;
+
+import java.util.Scanner;
+
+public class Main8 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int t = sc.nextInt();
+		
+		int[][] arr = new int[t][2];
+		
+		for (int i = 0; i < t; i++) {
+			arr[i][0] = sc.nextInt();
+			arr[i][1] = sc.nextInt();
+		}
+		
+		for (int j = 0; j < t; j++) {
+			System.out.printf("Case #%d: %d + %d = %d\n",
+					j+1, arr[j][0], arr[j][1], (arr[j][0]+arr[j][1]));
+		}
+		
+		sc.close();
+	}
+}
