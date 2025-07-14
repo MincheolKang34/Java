b75c4ce990d66312a92162454d957a1fa68ba342
mincheolkang34 <mincheolkang34@gmail.com>
2025-07-14 12:28:57 +0900

    add ch05 & SampleProject

--- ch05/src/sub2/EncapsuleTest.java
+++ ch05/src/sub2/EncapsuleTest.java
@@ -4,6 +4,12 @@ package sub2;
  * 날짜 : 2025.07.14.
  * 이름 : 강민철
  * 내용 : 캡슐화 실습하기
+ * 
+ * 캡슐화(정보은닉, Encapsulation)
+ *  - 캡슐화는 객체의 속성을 외부에서 참조하지 못하게 객체의 속성을 은닉하는 특성
+ *  - 클래스의 속성을 반드시 private으로 선언
+ *  - private 선언을 하면 생성자(Constructor)로 속성을 초기화
+ *  - 은닉된 정보(속성)을 안전하게 사용하기 위해 Getter, Setter 사용
  */
 public class EncapsuleTest {
 
