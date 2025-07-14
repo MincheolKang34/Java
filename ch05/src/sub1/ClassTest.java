b156c95026d888a88e8695b6c1df0cffd2bfdca6
mincheolkang34 <mincheolkang34@gmail.com>
2025-07-14 10:02:01 +0900

    add ch05

--- ch05/src/sub1/ClassTest.java
+++ ch05/src/sub1/ClassTest.java
@@ -1,11 +1,19 @@
 package sub1;
 
-import javax.net.ssl.SNIHostName;
-
 /*
  * 날짜 : 2025.07.14.
  * 이름 : 강민철
  * 내용 : 자바 객체지향 프로그래밍 클래스 실습하기
+ * 
+ * 스택(Stack)
+ *  - 자바의 일반변수, 참조변수, 매개변수가 push 되어 저장되는 메모리 영역
+ *  - 해당 메서드의 영역 및 프로그램이 종료되면 pop 되어 메모리에서 제거
+ *  - 객체(참조변수)는 스택에서 Heap 메모리 공간의 객체 인스턴스 참조(주소값 저장)
+ *  
+ * 힙(Heap)
+ *  - 자바의 객체가 생성되는 메모리 공간
+ *  - 스택처럼 순서대로 쌓이지 않고 랜덤으로 메모리 공간에 생성
+ *  - 참조변수가 제거되어 더 이상 참조되지 않으면 Garbage Collector로 해제됨.
  */
 public class ClassTest {
 
@@ -24,5 +32,18 @@ public class ClassTest {
 		sonata.speedDown(10);
 		sonata.show();
 		
+		// 객체 생성, 초기화, 상호작용
+		
+		Car avante = null; // 선언(declare)할 때는 일반적으로 null로 초기화
+		
+		avante = new Car();	// 생성
+		
+		avante.name = "아반테"; // 초기화 : 변수의 최초로 값을 대입
+		avante.color = "검정색";
+		avante.speed = 0;
+		
+		avante.speedUp(60);
+		avante.speedDown(20);
+		avante.show();
 	}
 }
