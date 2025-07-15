1b9d78c738b99c80cd06f7a29616f9fbfe0aba35
mincheolkang34 <mincheolkang34@gmail.com>
2025-07-15 11:06:13 +0900

    add CodingTest/src/step5/Main9

--- CodingTest/src/step5/Main9.java
+++ CodingTest/src/step5/Main9.java
@@ -8,18 +8,24 @@ public class Main9 {
 		
 		char[] s = sc.nextLine().toCharArray();
 		
+		char temp = 0;
 		for(int i=0; i<3; i++) {
-			char temp = s[i];
+			temp = s[i];
 			s[i] = s[s.length-1-i];
 			s[s.length-1-i] = temp;
 		}
 		
-		String[] str = s.toString().split(" ");
+		String[] str = String.valueOf(s).split(" ");
 		
 		int[] i = new int[2];
-		for(String stoi : str) {
-			
+		for(int j=0; j<2; j++) {
+			i[j] = Integer.parseInt(str[j]);
 		}
+		
+		if(i[0] > i[1])
+			System.out.println(i[0]);
+		else
+			System.out.println(i[1]);
 		
 		sc.close();
 	}
